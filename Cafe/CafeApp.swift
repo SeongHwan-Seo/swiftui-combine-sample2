c9527a02f3c791a95a92e538f9953e0940240e3a
seosh5704 <seosh5704@gmail.com>
2022-04-28 20:52:50 +0900

    Initial Commit

---
+++ Cafe/CafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeApp.swift
+//  Cafe
+//
+//  Created by seosh on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct CafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
